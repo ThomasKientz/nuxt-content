6167ee7353e03bd5847e0fa0cbb90fc09945acba
Thomas Kientz <thomas@kientz.fr>
2020-12-10 09:35:27 +0100

    add hello page

---
+++ pages/hello.vue
@@ -0,0 +1,29 @@
+<template>
+  <article>
+    <h1>{{ page.title }}</h1>
+    <nuxt-content :document="page" />
+    {{ rawContent }}
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content }) {
+    const page = await $content('hello').fetch()
+
+    return {
+      page
+    }
+  },
+
+  data: () => ({
+    rawContent: ''
+  })
+
+  // async created () {
+  //   const fileUrl = `_content/${this.page.path}${this.page.extension}`
+  //   this.rawContent = await fetch(fileUrl).then(res => res.text())
+  //   // console.log('file', file)
+  // }
+}
+</script>
